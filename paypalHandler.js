b8530c5867905efbdef92bf9a7f6c55af0d7abf7
philip06 <pcrilley06@gmail.com>
2021-10-31 23:19:52 -0400

    Refactor paypal handler to increase speed

--- paypalHandler.js
+++ paypalHandler.js
@@ -1,19 +1,21 @@
-const mCallbackPaypalContinue = (mutations, observer) => {
-	for (let mutation of mutations) {
-		if (mutation.type === 'childList') {
-			const submitButton = document.querySelector("#payment-submit-btn");
-			if (submitButton) {
-				console.log("Agree to paypal");
-				observer.disconnect();
-				buttonClicked = true;
-				submitButton.click();
-			}
-		}
-	}
+console.log("paypal integration handler script started");
+
+function rafAsync() {
+    return new Promise(resolve => {
+        requestAnimationFrame(resolve); //faster than set time out
+    });
 }
 
-const observerPaypalContinue = new MutationObserver(mCallbackPaypalContinue);
-observerPaypalContinue.observe(document, {
-	childList: true,
-	subtree: true
+function checkElement(selector) {
+    if (document.querySelector(selector) === null) {
+        return rafAsync().then(() => checkElement(selector));
+    } else {
+        return Promise.resolve(true);
+    }
+}
+
+
+checkElement('#payment-submit-btn') //use whichever selector you want
+.then(() => {
+	 document.querySelector("#payment-submit-btn").click();
 });
